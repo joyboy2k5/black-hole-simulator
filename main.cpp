a3573493ad3321638da28420eab661b4eda5413e
joyboy2k5 <saikrishnashylaja64@gmail.com>
2026-08-09 22:34:32 +0530

    Complete BHS-0.2 particle movement

--- main.cpp
+++ main.cpp
@@ -2,17 +2,39 @@
 
 int main()
 {
-    InitWindow(800, 600, "Black Hole Simulator - BHS 0.1");
+    InitWindow(800, 600, "Black Hole Simulator - BHS 0.2v");
+    float x=400;
+    float y=300;
+    float vx=100;
+    float vy=100;
+    
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
+        float dt = GetFrameTime();
 
         ClearBackground(BLACK);
+       
 
+        DrawCircle(x, y, 10, WHITE);
 
-        DrawCircle(400, 300, 10, WHITE);
+        if (IsKeyDown(KEY_RIGHT)){
+             x += vx * dt;
+        }
 
+        if (IsKeyDown(KEY_LEFT)){
+            x -= vx * dt;
+        }
+
+        if (IsKeyDown(KEY_DOWN)){
+            y += vy * dt;
+        }
+
+        if (IsKeyDown(KEY_UP)){
+            y -= vy * dt;
+        
+        }
         EndDrawing();
     }
 

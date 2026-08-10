361f57ea665543dbb39cd31adb84a2e9cc31c06f
joyboy2k5 <saikrishnashylaja64@gmail.com>
2026-08-10 23:58:37 +0530

    Add frame-independent braking and damping

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "raylib.h"
+#include <cmath>
 
 int main()
 {
-    InitWindow(1280, 720, "Black Hole Simulator - BHS 0.3");
+    InitWindow(1280, 720, "Black Hole Simulator - BHS 0.35");
 
     float x = 640;
     float y = 360;
@@ -21,7 +22,24 @@ int main()
 
         ClearBackground(BLACK);
 
-       
+        if (x>=1270 ){
+            x=1270;
+            vx=-vx;
+        }   
+        if (x<=10){
+            x=10;
+            vx=-vx;
+        }
+        if (y<=10){
+            y=10;
+            vy=-vy;
+        }
+        
+        if (y>=710){
+            y=710;
+            vy=-vy;
+        }
+
         if (IsKeyDown(KEY_RIGHT))
             ax = 50;
 
@@ -39,8 +57,18 @@ int main()
 
         else
             ay = 0;
+        if (IsKeyDown(KEY_SPACE)){
+            vx *= pow(0.4, dt);
+            vy *= pow(0.4, dt);
 
-       
+            if (vx > -0.1 && vx < 0.1)
+                vx = 0;
+
+            if (vy > -0.1 && vy < 0.1)
+                vy = 0;
+        }
+            
+        
         vx += ax * dt;
         vy += ay * dt;
 

61cef97aaefe7ac867713f8c514be80a074166b0
joyboy2k5 <saikrishnashylaja64@gmail.com>
2026-08-10 16:09:48 +0530

    Complete BHS-0.3 acceleration and physics

--- main.cpp
+++ main.cpp
@@ -2,39 +2,54 @@
 
 int main()
 {
-    InitWindow(800, 600, "Black Hole Simulator - BHS 0.2v");
-    float x=400;
-    float y=300;
-    float vx=100;
-    float vy=100;
-    
+    InitWindow(1280, 720, "Black Hole Simulator - BHS 0.3");
+
+    float x = 640;
+    float y = 360;
+
+    float vx = 0;
+    float vy = 0;
+
+    float ax = 50;
+    float ay = 50;
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
+
         float dt = GetFrameTime();
 
         ClearBackground(BLACK);
+
        
+        if (IsKeyDown(KEY_RIGHT))
+            ax = 50;
+
+        else if (IsKeyDown(KEY_LEFT))
+            ax = -50;
+
+        else
+            ax = 0;
+
+        if (IsKeyDown(KEY_DOWN))
+            ay = 50;
+
+        else if (IsKeyDown(KEY_UP))
+            ay = -50;
+
+        else
+            ay = 0;
+
+       
+        vx += ax * dt;
+        vy += ay * dt;
+
+       
+        x += vx * dt;
+        y += vy * dt;
 
         DrawCircle(x, y, 10, WHITE);
 
-        if (IsKeyDown(KEY_RIGHT)){
-             x += vx * dt;
-        }
-
-        if (IsKeyDown(KEY_LEFT)){
-            x -= vx * dt;
-        }
-
-        if (IsKeyDown(KEY_DOWN)){
-            y += vy * dt;
-        }
-
-        if (IsKeyDown(KEY_UP)){
-            y -= vy * dt;
-        
-        }
         EndDrawing();
     }
 
